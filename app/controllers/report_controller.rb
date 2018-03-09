872ffff4337b0c52fd4eed7a97318685170bdf65
Amit Kumar <amitk@eatechnologies.com>
2018-03-09 18:20:22 +0530

    Report filter

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,10 @@
 class ReportController < ApplicationController
   def index
-  	@tasks = Task.where(:company_id => current_user.company_id).order("id desc")
+  	@search = Task.where(:company_id => current_user.company_id).ransack(view_context.empty_blank_params params[:q])
+    @tasks = @search.result(distinct: true)
+                        .order("id DESC")
+    
+    @task_form_tag_values = TaskFormTagValue.where(:company_id => current_user.company_id)
+  	# @tasks = Task.where(:company_id => current_user.company_id).order("id desc")
   end
 end
