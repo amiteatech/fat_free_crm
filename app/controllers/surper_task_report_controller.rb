96f236c0c8cac05d3144ebd5518f471c6dfd52b4
Bibek Singh <bibek@eatechnologies.com>
2018-05-23 16:03:18 +0530

    changes for bugs

--- app/controllers/surper_task_report_controller.rb
+++ app/controllers/surper_task_report_controller.rb
@@ -1,13 +1,9 @@
 class SurperTaskReportController < ApplicationController
   def task_super
-  	@tasks = Task.all
-    @bucket = Setting.unroll(:task_bucket)[1..-1] << [t(:due_specific_date, default: 'On Specific Date...'), :specific_time]
-
+  	@tasks = Task.all.order("id DESC")
   end
 
   def report_super
-
   	@tasks = Task.all.order("id DESC")
-   
   end
 end
